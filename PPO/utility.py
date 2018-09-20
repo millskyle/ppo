0692314db2a540dc6e7df805a6247795ee9ad40f
Kyle Mills <kyle.mills@uoit.net>
2018-09-20 16:53:27 -0700

    get_log_path now creates dir if not exists

--- PPO/utility.py
+++ PPO/utility.py
@@ -1,6 +1,10 @@
 import os
 
 def get_log_path(logdir, prefix='run_'):
+    try:
+        os.mkdirs(logdir)
+    except:
+        pass
     nums = [ int(s.replace(prefix, '')) for s in os.listdir(logdir) ]
     if len(nums) > 0:
         return os.path.abspath(logdir) + '/' + prefix + str(max(nums)+1).zfill(2)
