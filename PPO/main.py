781a60d647d585767a6a097e590f4e2f224154d6
Kyle Mills <kyle.mills@uoit.net>
2018-09-24 12:15:09 -0700

    Curiosity implemented and running. No evidence that it works yet though.

--- PPO/main.py
+++ PPO/main.py
@@ -22,11 +22,13 @@ SOLVED_THRESHOLD_CONSECUTIVE_ITERATIONS = 100
 CHKPT_PATH = './model/'
 
 RESTORE = True
+CURIOSITY = True
+ETA = 1.
 
 
-env = gym.make('Acrobot-v1')
+#env = gym.make('MountainCar-v0')
 #env = gym.make('Stirling-v0')
-#env = gym.make('CartPole-v0')
+env = gym.make('CartPole-v0')
 #env = gym.make('RoboschoolPong-v1')
 
 if __name__=='__main__':
@@ -35,7 +37,9 @@ if __name__=='__main__':
     policy = NeuralNet(env=env, label='policy')
     old_policy = NeuralNet(env=env, label='old_policy')
 
-    ppo = Algorithm(policy=policy, old_policy=old_policy, gamma=0.95, epsilon=0.2, c_1=0.1, c_2=1.0)
+    ppo = Algorithm(policy=policy, old_policy=old_policy, gamma=0.95,
+                    epsilon=0.2, c_1=0.1, c_2=1.0,
+                    use_curiosity=CURIOSITY, eta=ETA)
 
     saver = tf.train.Saver()
 
@@ -51,15 +55,17 @@ if __name__=='__main__':
         ppo.attach_session(sess)
 
         obs = env.reset()
-        reward = 0
+        reward_E = 0
         success_num = 0
 
 
         for iteration in range(ITERATIONS):
-            observations =[]
+            observations = []
+            observations_tp1 = []
             actions = []
             v_preds = []
-            rewards = []
+            rewards_extrinsic = []
+            rewards_intrinsic = []
             run_policy_steps = 0
 
             ppo._start_of_episode()
@@ -74,9 +80,18 @@ if __name__=='__main__':
                 observations.append(obs)
                 actions.append(action)
                 v_preds.append(v_pred)
-                rewards.append(reward)
+                rewards_extrinsic.append(reward_E)
 
-                next_obs, reward, done, info = env.step(action)
+                next_obs, reward_E, done, info = env.step(action)
+
+                observations_tp1.append(next_obs)
+
+                # for curiosity, apply the intrinsic reward
+                if CURIOSITY:
+                    reward_I = ppo.evaluate_intrinsic_reward(obs=obs, obs_tp1=next_obs)
+                    rewards_intrinsic.append(reward_I)
+                else:
+                    rewards_intrinsic.append(0.0)
 
                 if ppo._render:
                     env.render()
@@ -87,16 +102,16 @@ if __name__=='__main__':
                 if done:
                     v_preds_next = v_preds[1:] + [0]  # next state of terminate state has 0 state value
                     obs = env.reset()
-                    rewards = [float(i) / run_policy_steps for i in rewards]
-                    reward = 0
-                    break
+                    reward_E = 0
                     ppo._end_of_episode()
                     break
 
                 else:
                     obs = next_obs
+
+
             logging.info("EPISODE {0:5d}".format(iteration))
-            if sum(rewards) >= SOLVED_THRESHOLD:
+            if sum(rewards_extrinsic) >= SOLVED_THRESHOLD:
                 success_num += 1
                 if success_num >= SOLVED_THRESHOLD_CONSECUTIVE_ITERATIONS:
                     saver.save(sess, CHKPT_PATH + 'final.chkpt')
@@ -107,15 +122,21 @@ if __name__=='__main__':
                 success_num = 0
 
 
+            rewards = [i+e for i,e in zip(rewards_extrinsic, rewards_intrinsic)]
 
             advantage_estimate = ppo.estimate_advantage(rewards=rewards,
                                                         v_preds=v_preds,
                                                         v_preds_next=v_preds_next)
-            ppo._summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag='rewards', simple_value=sum(rewards))]), iteration)
+            with tf.variable_scope("Rewards"):
+                ppo._summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag='rewards_I', simple_value=sum(rewards_intrinsic))]), iteration)
+                ppo._summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag='rewards_E', simple_value=sum(rewards_extrinsic))]), iteration)
+
 
             observations = np.reshape(observations, newshape=[-1] + list(obs_space.shape))
+            observations_tp1 = np.reshape(observations_tp1, newshape=[-1] + list(obs_space.shape))
             actions = np.array(actions).astype(np.int32)
             rewards = np.array(rewards).astype(np.float32)
+
             v_preds_next = np.array(v_preds_next).astype(np.float32)
             advantage_estimate = np.array(advantage_estimate).astype(np.float32)
             advantage_estimate = (advantage_estimate - advantage_estimate.mean()) / advantage_estimate.std()
@@ -124,7 +145,7 @@ if __name__=='__main__':
 
             if iteration > 0 and iteration % 1 == 0:
 
-                data = [observations, actions, rewards, v_preds_next, advantage_estimate]
+                data = [observations, actions, rewards, v_preds_next, advantage_estimate, observations_tp1]
 
                 for batch in range(4):
                     sample_indices = np.random.randint(low=0, high=observations.shape[0], size=128)
@@ -134,5 +155,6 @@ if __name__=='__main__':
                               actions=data_sample[1],
                               rewards=data_sample[2],
                               v_preds_next=data_sample[3],
-                              advantage_estimate=data_sample[4]
+                              advantage_estimate=data_sample[4],
+                              observations_tp1=data_sample[5],
                              )
--- PPO/ppo.py
+++ PPO/ppo.py
@@ -17,11 +17,13 @@ class Algorithm(object):
         self.advantage_estimate = tf.placeholder(dtype=tf.float32, shape=[None], name='advantage_estimate')
 
 
-    def __init__(self, policy, old_policy, gamma=0.95, epsilon=0.2, c_1=1., c_2=0.01):
+    def __init__(self, policy, old_policy, gamma=0.95, epsilon=0.2, c_1=1., c_2=0.01,
+                 use_curiosity=False, eta=0.1):
         """ epsilon :: clip_value """
         self.policy = policy
         self.old_policy = old_policy
         self.gamma = gamma
+        self._use_curiosity = use_curiosity
 
         self.__weight_update_counter = 0
 
@@ -42,41 +44,51 @@ class Algorithm(object):
         act_probs_old = tf.reduce_sum(act_probs_old, axis=1)
 
 
-
-
-
         ### Curiosity
-        CURIOSITY = False
-        if CURIOSITY:
-            a_t = self.policy.a_prob
-            s_t = self.policy.observation
-            s_tp1 = self.policy.observation_tp1
+        if self._use_curiosity:
+            with tf.variable_scope('curiosity'):
+                a_t = self.policy.a_prob
+                s_t = self.policy.observation
+                self.observation_tp1 = tf.placeholder(dtype=tf.float32,
+                                                  shape=self.policy.observation.shape,
+                                                  name='observation_tp1')
+                s_tp1 = self.observation_tp1
 
-            #encode the observation. This could be any type of neural net.
-            #if the observation is an image, probably want convolutional
-            inverse_nn_t = DenseNN(in_=s_t,
-                                 units=[32,64,128],
-                                 activations=[tf.nn.selu,]*3,
-                                 scope='curiosity_inverse')
-            #encode the observation at time t+1 with the same neural net (and
-            #same weights)
-            inverse_nn_tp1 = DenseNN(in_=s_tp1,
-                                      units=[32,64,128],
-                                      activations=[tf.nn.selu,]*3,
-                                      scope='curiosity_inverse')
-            joined = tf.concatenate((inverse_nn_t, inverse_nn_tp1), axis=1)
+                #encode the observation. This could be any type of neural net.
+                #if the observation is an image, probably want convolutional
+                inverse_nn_t = DenseNN(in_=s_t,
+                                     units=[32,64,128],
+                                     activations=[tf.nn.selu,]*3,
+                                     scope='curiosity_inverse')
+                #encode the observation at time t+1 with the same neural net (and
+                #same weights)
+                inverse_nn_tp1 = DenseNN(in_=s_tp1,
+                                          units=[32,64,128],
+                                          activations=[tf.nn.selu,]*3,
+                                          scope='curiosity_inverse')
 
-            inverse_nn = DenseNN(in_=joined,
-                                 units=[256,a_t.shape[1]],
-                                 activations=[tf.nn.selu,None],
-                                 scope='curiosity_inverse)
+                joined = tf.concat((inverse_nn_t.output, inverse_nn_tp1.output), axis=1)
 
+                inverse_nn = DenseNN(in_=joined,
+                                     units=[256,a_t.shape[1]],
+                                     activations=[tf.nn.selu,None],
+                                     scope='curiosity_inverse_enc')
+                a_pred = inverse_nn.output
+                inp_forward = tf.concat((a_pred, s_t), axis=1)
+                forward_nn = DenseNN(in_=inp_forward,
+                                     units=[256, s_t.shape[1]],
+                                     activations=[tf.nn.selu, None],
+                                     scope='curiosity_forward'
+                                     )
+        if self._use_curiosity:
+            #we want to train the inverse network to predict the correct action:
+            with tf.variable_scope('L/Inverse'):
+                L_I = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=a_pred,
+                                                             labels=a_t))
+            with tf.variable_scope('L/Forward'):
+                L_F = tf.reduce_mean(tf.nn.l2_loss(forward_nn.output-s_tp1))
 
-
-
-
-
-
+            self.reward_intrinsic = eta * L_F
 
 
         with tf.variable_scope('L/CLIP'):
@@ -89,8 +101,6 @@ class Algorithm(object):
                                   tf.multiply(ratio_clipped, self.advantage_estimate)
                              )
                         )
-
-
 
         with tf.variable_scope('L/VF'):
             L_vf = tf.reduce_mean(tf.square(
@@ -115,12 +125,17 @@ class Algorithm(object):
             loss = -(L_clip - c_1*L_vf + c_2*L_S)
             #The paper says to MAXIMIZE this loss, so let's minimize the
             #negative instead
-
-        #for var in self.old_policy.get_variables(trainable_only=True):
-        #    tf.summary.histogram(var.name, var)
+            if self._use_curiosity:
+                loss = loss + L_I + L_F
+                tf.summary.scalar('L_inverse', L_I)
+                tf.summary.scalar('L_forward', L_F)
 
         for var in self.policy.get_variables(trainable_only=True):
             tf.summary.histogram(var.name, var)
+
+        if self._use_curiosity:
+            for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='curiosity'):
+                tf.summary.histogram(var.name, var)
 
         with tf.variable_scope('optimizer'):
             optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, epsilon=1e-5)
@@ -141,17 +156,31 @@ class Algorithm(object):
         self._summary_writer = tf.summary.FileWriter(get_log_path('./logs','run_'),
                                                      self._sess.graph, flush_secs=5)
 
+    def evaluate_intrinsic_reward(self, obs, obs_tp1  ):
+        obs_d = np.array(obs).reshape(1,-1)
+        obs_tp1_d = np.array(obs).reshape(1,-1)
+        r_I = self.sess.run(self.reward_intrinsic, feed_dict={self.policy.observation:obs_d,
+                                                              self.observation_tp1:obs_tp1_d})
+        return r_I
 
-    def train(self, observations, actions, rewards, v_preds_next, advantage_estimate):
+    def train(self, observations, actions, rewards, v_preds_next, advantage_estimate, observations_tp1):
         logging.debug("Updating weights")
-        _summary, _ = self.sess.run([self._summaries, self.train_op], feed_dict={
-                                                    self.policy.observation: observations,
-                                                    self.old_policy.observation: observations,
-                                                    self.actions: actions,
-                                                    self.rewards: rewards,
-                                                    self.v_preds_next: v_preds_next,
-                                                    self.advantage_estimate: advantage_estimate
-                                                })
+
+        feed_dict = {
+                        self.policy.observation: observations,
+                        self.old_policy.observation: observations,
+                        self.actions: actions,
+                        self.rewards: rewards,
+                        self.v_preds_next: v_preds_next,
+                        self.advantage_estimate: advantage_estimate
+                    }
+        if self._use_curiosity:
+            feed_dict[self.observation_tp1] = observations_tp1
+
+        #run the training op, get summaries
+        _summary, _ = self.sess.run([self._summaries, self.train_op], feed_dict=feed_dict)
+
+
         self.__weight_update_counter += 1
         if self.__weight_update_counter%10==0:
             self._summary_writer.add_summary(_summary, self.__weight_update_counter)
