Fixed incorrect gamma calculation in multistep

DQN/dqn.py
@@ -17,19 +17,16 @@ class Placeholders(object):
         self.action_in = tf.placeholder(tf.int64, shape=[None,], name='action_in')
         self.reward_in = tf.placeholder(tf.float32, shape=[None,], name='reward_in')
         self.done_in = tf.placeholder(tf.bool, shape=[None,], name='done_in')
-
-
-
+        self.gamma = tf.placeholder(tf.float32, shape=[], name='gamma')
 
 
 
 class DQN(object):
-    def __init__(self, env, restore=True, checkpoint_path=None, gamma=0.95, flags={}):
+    def __init__(self, env, restore=True, checkpoint_path=None, flags={}):
 
         self._env = env
         self.__restore = restore
         self._checkpoint_path = checkpoint_path
-        self._gamma = gamma
         self._flags = flags
         self._epsilon_override = None
 
@@ -81,7 +78,7 @@ class DQN(object):
         q_value_of_the_action_we_took = tf.reduce_sum(tf.one_hot(self._ph.action_in, depth=self.online_Qnet.output.shape[1])*self.online_Qnet.output, axis=1)
 
         #y_j in the DQN paper:
-        yj = rj + done_mask * self._gamma * Q_value_targ
+        yj = rj + done_mask * self._ph.gamma * Q_value_targ
 
         #td-error:
         self._td_error = yj - q_value_of_the_action_we_took
@@ -158,15 +155,15 @@ class DQN(object):
                                                      self._sess.graph, flush_secs=1)
         self._merged_summaries = tf.summary.merge_all()
 
-    def discount_rewards(self, R):
+    def discount_rewards(self, gamma, R):
         """ R is a list of rewards to be discounted with R[-1]
             being the most into the future (i.e. most heavily-discounted).
             R[0] is not discounted
         """
-        R_ = np.array(R)*self._gamma**np.arange(len(R))
+        R_ = np.array(R) * gamma**np.arange(len(R))
         return R_
 
-    def train(self, batch_size, epsilon, debug=False):
+    def train(self, batch_size, epsilon, gamma, debug=False):
         data = self._replay_buffer.sample(N=batch_size, mode='prioritized')
 
         s, a, r, d, s_tp1  = list(map(list, zip(*data))) #transpose the list of lists
@@ -182,6 +179,7 @@ class DQN(object):
             self._ph.reward_in:    np.array(r),
             self._ph.done_in:      np.array(d),
             self._ph.state_tp1_in: s_tp1,
+            self._ph.gamma: gamma,
         }
 
         if self._weight_update_counter.eval() % 100 == 0:

DQN/main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import copy
 import numpy as np
 from dqn import DQN
 import gym
@@ -32,11 +33,12 @@ epsilon_from_file = ParameterOverrideFile(name='epsilon', refresh_frequency=0.1)
 
 FLAGS = {'prioritized_buffer': True,
          'double_q_learning': True,
-         'multi_steps_n': 3,
-         'name_prefix': 'ms3_',
+         'multi_steps_n': 1,
+         'name_prefix': 'ms1_',
 
          'state_seq_length': 1,
          'replay_buffer_size': 5000,
+        'gamma': 0.95,
 
         #Neural network stuff:
         'learning_rate': 1e-4,
@@ -44,7 +46,7 @@ FLAGS = {'prioritized_buffer': True,
         }
 
 env = gym.make('CartPole-v0')
-env = gym.make('KBlocker-v0')
+#env = gym.make('KBlocker-v0')
 
 
 
@@ -84,24 +86,22 @@ if __name__=='__main__':
                 dqn._sequence_buffer.add(next_obs)
                 obs_seq_tp1, _ = dqn._sequence_buffer.dump()
 
-
                 dqn._multi_steps_buffer.add((obs_seq, action, reward, done, obs_seq_tp1), add_until_full=False)
                 if dqn._multi_steps_buffer.is_full:
                     _ds, _ps = dqn._multi_steps_buffer.dump()  # get current contents of buffer, don't modify
-                    assert len(_ds) == FLAGS['multi_steps_n'], "ERROR"
                     _rewards = [_ds[i][2] for i in range(len(_ds))]  #extract just the rewards (the third column)
-                    _returns = dqn.discount_rewards(_rewards) #discount the rewards
-                    _return = np.sum(_returns)
-                    _otpN = _ds[-1][-1]
+                    _returns = dqn.discount_rewards(R=_rewards, gamma=FLAGS['gamma']) #discount the rewards
+                    _return = np.sum(_returns) 
+                    _otpN = copy.deepcopy(_ds[-1][-1])  #t+N is the last column of the last row
 
-                    _d, _p = dqn._multi_steps_buffer.popleft(1)
-                    _o, _a, _r, _d, _otp1 = _d[0]
+                    _data, _ = dqn._multi_steps_buffer.popleft(1)
+                    _o, _a, _r, _d, _otp1 = _data[0]
                     dqn._replay_buffer.add((_o, _a, _return, _d, _otpN), add_until_full=False)
 
                 if dqn._total_step_counter.eval()%TRAINING_FREQ==0:
                     if dqn._replay_buffer.size > BATCH_SIZE:
                         bar.status("Training")
-                        dqn.train(BATCH_SIZE, epsilon=this_epsilon)
+                        dqn.train(BATCH_SIZE, epsilon=this_epsilon, gamma=FLAGS['gamma']**FLAGS.get('multi_steps_n',1.0))
                     else:
                         bar.status("Filling replay buffer")
                         logging.debug("Filling replay buffer with experiences - size: {}".format(dqn._replay_buffer.size))